d0315a088f6ac739dfe9d4cd65d9174593d1f69e
sonias02 <sonias02@cs.washington.edu>
2022-05-31 16:06:44 -0700

    delete getFieldsModel from Central and Aggregate (was moved to layer2form)

--- services/Central.py
+++ services/Central.py
@@ -175,62 +175,6 @@ class Central (Service):
         return response
 
 
-    def getFieldsModel(self,currentLayer):
-        fieldsModel = []
-        g_type= currentLayer.geometryType()
-        fieldDef={'name':'GEOMETRY','type':'geopoint','bind':{'required':'true()'}}
-        fieldDef['Appearance']= 'maps'
-        if g_type==0:
-            fieldDef['label']='add point location'
-        elif g_type==1:
-            fieldDef['label']='Draw Line'
-            fieldDef['type']='geotrace'
-        else:
-            fieldDef['label']='Draw Area'
-            fieldDef['type']='geoshape'
-        fieldsModel.append(fieldDef)
-        i=0
-        for field in currentLayer.fields():
-            widget =currentLayer.editorWidgetSetup(i)
-            fwidget = widget.type()
-            if (fwidget=='Hidden'):
-                i+=1
-                continue
-
-            fieldDef = {}
-            fieldDef['name'] = field.name()
-            fieldDef['map'] = field.name()
-            fieldDef['label'] = field.alias() or field.name()
-            fieldDef['hint'] = ''
-            fieldDef['type'] = self.QVariantToODKtype(field.type())
-            fieldDef['bind'] = {}
-#            fieldDef['fieldWidget'] = currentFormConfig.widgetType(i)
-            fieldDef['fieldWidget']=widget.type()
-            self.print('getFieldModel',fieldDef['fieldWidget'])
-            if fieldDef['fieldWidget'] in ('ValueMap','CheckBox','Photo','ExternalResource'):
-                if fieldDef['fieldWidget'] == 'ValueMap':
-                    fieldDef['type']='select one'
-                    valueMap=widget.config()['map']
-                    config={}
-                    for value in valueMap:
-                        for k,v in value.items():
-                                config[v]=k
-                    self.print('configuration is ',config)
-                    choicesList=[{'name':name,'label':label} for name,label in config.items()]
-                    fieldDef["choices"] = choicesList
-                elif fieldDef['fieldWidget'] == 'Photo' or fieldDef['fieldWidget'] == 'ExternalResource' :
-                    fieldDef['type']='image'
-                    self.print('got an image type field')
-
-#                fieldDef['choices'] = config
-            else:
-                fieldDef['choices'] = {}
-            if fieldDef['name'] == 'ODKUUID':
-                fieldDef["bind"] = {"readonly": "true()", "calculate": "concat('uuid:', uuid())"}
-            fieldsModel.append(fieldDef)
-            i+=1
-        return fieldsModel
-
     def updateLayerXML(self,layer,xml):
         geoField=''
         ns='{http://www.w3.org/2002/xforms}'
